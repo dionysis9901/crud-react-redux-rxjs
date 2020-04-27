af7841359bf411725c49f52f4c73f01d1e9865c4
dionysis9901 <dionysis.koufis@gmail.com>
2020-04-27 19:12:58 +0300

    adds a Completed Effect For The Todos

--- src/components/todo/Todo.js
+++ src/components/todo/Todo.js
@@ -6,36 +6,72 @@ import {
   userOpenRenameScreen,
 } from "../../actions";
 import { useDispatch } from "react-redux";
-import { BsFillTrashFill, BsGear } from "react-icons/bs";
+import { AiFillEdit, AiTwotoneDelete } from "react-icons/ai";
 const Todo = ({ userId, id, title, completed }) => {
   const dispatch = useDispatch();
-  return (
-    <div className="todo">
-      <p className="todo__title">{title}</p>
-      <input
-        type="checkbox"
-        className="todo__status"
-        defaultChecked={completed ? true : false}
-        onChange={(event) => {
-          return dispatch(completeStatusUpdate(completed, id, title));
-        }}
-      />
-      <div className="todo__buttons"></div>
-      <button
-        className="todo__buttons__btn"
-        onClick={() => {
-          return dispatch(userOpenRenameScreen(id, completed));
-        }}
-      >
-        <BsGear />
-      </button>
-      <button
-        className="todo__buttons__btn"
-        onClick={() => dispatch(deleteTodo(id))}
-      >
-        <BsFillTrashFill />
-      </button>
-    </div>
-  );
+  if (completed) {
+    return (
+      <div className="todo inactive">
+        <p className="todo__title inactive__title">{title}</p>
+        <input
+          type="checkbox"
+          className="todo__status"
+          defaultChecked={completed ? true : false}
+          onChange={(event) => {
+            return dispatch(completeStatusUpdate(completed, id, title));
+          }}
+        />
+        <div className="todo__buttons todo__inactive__buttons">
+          <button
+            disabled={true}
+            className="inactive__inactiveBtn"
+            onClick={() => {
+              return dispatch(userOpenRenameScreen(id, completed));
+            }}
+          >
+            <AiFillEdit />
+          </button>
+          <button
+            disabled={true}
+            className="inactive__inactiveBtn"
+            onClick={() => dispatch(deleteTodo(id))}
+          >
+            <AiTwotoneDelete />
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="todo active">
+        <p className="todo__title">{title}</p>
+        <input
+          type="checkbox"
+          className="todo__status"
+          defaultChecked={completed ? true : false}
+          onChange={(event) => {
+            return dispatch(completeStatusUpdate(completed, id, title));
+          }}
+        />
+        <div className="todo__buttons">
+          {" "}
+          <button
+            className="todo__buttons__btn"
+            onClick={() => {
+              return dispatch(userOpenRenameScreen(id, completed));
+            }}
+          >
+            <AiFillEdit />
+          </button>
+          <button
+            className="todo__buttons__btn"
+            onClick={() => dispatch(deleteTodo(id))}
+          >
+            <AiTwotoneDelete />
+          </button>
+        </div>
+      </div>
+    );
+  }
 };
 export default Todo;
